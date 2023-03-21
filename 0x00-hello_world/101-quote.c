f3acd3b263ae563a5b6c85e4f2c0158c14cbcfb0
NADIR <bentaleb.nadir7@gmail.com>
2023-03-20 18:35:16 -0700

    See ya

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
-#include<unistd.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  * Description: print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
  * Return: return an error 1.
  */
 

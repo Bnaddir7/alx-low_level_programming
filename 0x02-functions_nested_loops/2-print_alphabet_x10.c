a47c55b93cb9a5276ac5cc5f0bd7d85f30ba2b9b
NADIR <bentaleb.nadir7@gmail.com>
2023-03-21 18:12:05 -0700

    Add some program

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * main - Entry point.
+ * Description: function that prints 10 times the alphabet.
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int line, i;
+	for (line = 0; line <= 9; ++line)
+	{
+		for (i = 'a'; i <= 'z'; ++i)
+			_putchar(i);
+		_putchar('\n');
+	}
+}

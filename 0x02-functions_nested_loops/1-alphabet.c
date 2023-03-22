bd1c98f1775752cae9f8626b1651f9624e63ab64
NADIR <bentaleb.nadir7@gmail.com>
2023-03-21 18:20:41 -0700

    Fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,9 +5,11 @@
  * Description: a function that prints the alphabet in lowercase.
  * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; ++i)
 	{
 		_putchar(i);
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,10 +4,13 @@
  * main - Entry point.
  * Description: function that prints 10 times the alphabet.
  * Return: Always 0.
+ *
  */
+
 void print_alphabet_x10(void)
 {
 	int line, i;
+
 	for (line = 0; line <= 9; ++line)
 	{
 		for (i = 'a'; i <= 'z'; ++i)

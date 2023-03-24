f50c74ef2fb138615ac3f7368597aa302e0543e4
NADIR <bentaleb.nadir7@gmail.com>
2023-03-23 19:31:34 -0700

    Good

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Function that checks fro uppercase
+ * Return: if true 1 else 0
+ *
+ */
+
+int _isupper(int c);
+{
+	if (c >= 65; c <= 90; c++)
+		return (1);
+	return (0);
+}

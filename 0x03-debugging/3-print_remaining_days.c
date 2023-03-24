20c4ba14efb4178a7872338a1407db8667133200
NADIR <bentaleb.nadir7@gmail.com>
2023-03-23 18:18:09 -0700

    Fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,7 +8,6 @@
  * @day: day of month
  * @year: year
  */
-
 void print_remaining_days(int month, int day, int year)
 {
 		/**
@@ -37,4 +36,3 @@ void print_remaining_days(int month, int day, int year)
 			}
 		}
 }
-

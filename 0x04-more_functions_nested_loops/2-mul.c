c6e687611cd82bd1e51e04b848cd776388ed8dd7
NADIR <bentaleb.nadir7@gmail.com>
2023-03-24 18:04:18 -0700

    Yeep

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - function that multi
+ * @a: takes input
+ * @b: takes input
+ * Return: results
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers -F that prints a specail nums
+ *
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n == 2 || n == 4)
+		{
+			continue;	/* skip printing 2 and 4 */
+		}
+		_putchar(n + 48);
+	}
+	_putchar("\n")
+}

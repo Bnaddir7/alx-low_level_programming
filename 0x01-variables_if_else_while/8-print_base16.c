a3ec95ff2f8c0f7810f5be6494a5a281ca819e67
NADIR <bentaleb.nadir7@gmail.com>
2023-04-02 16:24:21 -0700

    8th task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c <= 102)
+	{
+		putchar(c);
+		if (c == 57)
+			c += 39;
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}

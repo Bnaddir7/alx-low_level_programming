0866532a430576e8c3c4736deb7b81b853f491cf
NADIR <bentaleb.nadir7@gmail.com>
2023-04-02 18:01:37 -0700

    Fix error

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - F adds two int
+ * @a: num1
+ * @b: num2
+ * Return: results
+ */
+int add(int a, int b)
+{
+	int R = a + b;
+
+	a = 89;
+	b = 9;
+	return (R);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: takes all natural numbers
+ */
+void print_to_98(int n)
+{
+	if (n = 0; n <=98) && (n++)
+	{
+		_putchar(n + 48);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}

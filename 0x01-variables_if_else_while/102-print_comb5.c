759d134f68f134bfc6919629691525e42e0903f3
NADIR <bentaleb.nadir7@gmail.com>
2023-04-02 17:53:10 -0700

    Last advanced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 0, i;
+
+	while (n <= 99)
+	{
+		i = n;
+		while (i <= 99)
+		{
+			if (i != n)
+			{
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				putchar(' ');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+
+				if (i != 98 || i != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			i++;
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}

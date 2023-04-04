a32b4f6cb1e7c79e6efbe05aa3888f85d84f7e53
NADIR <bentaleb.nadir7@gmail.com>
2023-04-03 18:18:25 -0700

    2nd task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a char in a string
+ *
+ * @s: The string
+ * @c: character
+ *
+ * Return: Returns a pointer to c in s,
+ *	or NULL if c is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return ('\0');
+}

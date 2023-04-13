0cfa6ff8018ef9802ce33b6cfaabe14bee973c71
NADIR <bentaleb.nadir7@gmail.com>
2023-04-13 00:36:45 -0700

    2nd Task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: return NULL if fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int s1_len = 0, s2_len = 0;
+	char *result;
+	int i, j;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[s1_len] != '\0')
+	{
+		s1_len++;
+	}
+	while (s2[s2_len] != '\0')
+	{
+		s2_len++;
+	}
+	result = malloc((s1_len + (s2_len + 1)) * sizeof(char));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < s1_len; i++)
+	{
+		result[i] = s1[i];
+	}
+	for (j = 0; j < s2_len; j++)
+	{
+		result[i + j] = s2[j];
+	}
+	result[i + j] = '\0';
+
+	return (result);
+}
